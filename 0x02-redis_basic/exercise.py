65b57d22dceb2ad6a2f9514bf205da4eef96c018
mideactive <jenyoolumide@yahoo.com>
2023-10-18 12:31:39 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,11 +4,12 @@ Main file
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+
 
 class Cache:
     """
-    Cache class to store data in Redis
+    Cache class to store and retrieve data from Redis
     """
 
     def __init__(self):
@@ -23,12 +24,39 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Callable = None) -> Union[str, int, bytes]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieve data from Redis as a string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve data from Redis as an integer
+        """
+        return self.get(key, fn=int)
+
+
 if __name__ == '__main__':
     cache = Cache()
 
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
 
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
